23586701bcca1535c92844a8fec556700b858900
Adrian Eyre <adrian.eyre@slimmingworld.co.uk>
2019-09-16 08:41:30 +0100

    CI Setup

---
+++ deploy.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const FtpClient = require('ftp');
+const glob = require('glob');
+
+const basePath = './build';
+const destinationPath = '/public_html';
+const config = {
+    host: process.env.FTP_HOST,
+    password: process.env.FTP_PASSWORD,
+    user: process.env.FTP_USER,
+};
+
+const ftpClient = new FtpClient();
+
+function createDirectory(destination) {
+    return ftpClient.mkdir(destination, true, (error) => {
+        if (error) throw error;
+
+        ftpClient.end();
+    });
+}
+
+function uploadFile(file, destination) {
+    ftpClient.put(file, destination, (error) => {
+        if (error) throw error;
+
+        console.log(`${file} => ${destination}`);
+        ftpClient.end();
+    });
+}
+
+function handlePath(path) {
+    const relativeFile = path.replace(`${basePath}/`, '');
+    const destination = `${destinationPath}/${relativeFile}`;
+
+    if (fs.lstatSync(path).isDirectory()) {
+        return createDirectory(destination);
+    }
+
+    return uploadFile(path, destination);
+}
+
+ftpClient.on('ready', () => {
+    glob.sync(`${basePath}/**/*`).forEach(handlePath);
+});
+
+ftpClient.connect(config);
