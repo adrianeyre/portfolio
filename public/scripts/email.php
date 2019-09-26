9b7eea31cda6fa07cbc24fdf140e256afc917b92
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2019-09-26 16:55:02 +0100

    Reformatting of email sender

--- public/scripts/email.php
+++ public/scripts/email.php
@@ -1,62 +1,47 @@
 <?php
-    if(isset($_POST['email'])) {
-     
-        $email_to = "info@adrianeyre.co.uk";
-        $email_subject = "Message from portfolio site";
-     
-        function died($error) {
-            echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-            echo "These errors appear below.<br /><br />";
-            echo $error."<br /><br />";
-            echo "Please go back and fix these errors.<br /><br />";
-            die();
-        }
-     
-        if(!isset($_POST['name']) ||
-            !isset($_POST['email']) ||
-            !isset($_POST['message']) {
-            died('We are sorry, but there appears to be a problem with the form you submitted.');       
-        }
-     
-        $name = $_POST['name'];
-        $email_from = $_POST['email'];
-        $message = $_POST['message'];
-     
-        $error_message = "";
-        $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-     
-      if (!preg_match($email_exp, $email_from)) {
-        $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-      }
-     
-      if (strlen($message) < 2) {
-        $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-      }
-     
-      if (strlen($error_message) > 0) {
-        died($error_message);
-      }
-     
-    $email_message = "Form details below.\n\n";
- 
-     
-    function clean_string($string) {
-      $bad = array("content-type", "bcc:", "to:", "cc:", "href");
-      return str_replace($bad, "", $string);
-    }
- 
-    $email_message .= "Name: ".clean_string($email)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Message: ".clean_string($message)."\n";
- 
-    $headers = 'From: '.$email_from."\r\n".
-    'Reply-To: '.$email_from."\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-    @mail($email_to, $email_subject, $email_message, $headers);  
+
+	function died($error) {
+		echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+		echo "These errors appear below.<br /><br />";
+		echo $error."<br /><br />";
+		echo "Please go back and fix these errors.<br /><br />";
+		die();
+	}
+
+	function clean_string($string) {
+		$bad = array("content-type", "bcc:", "to:", "cc:", "href");
+		return str_replace($bad, "", $string);
+	}
+
+	if (!isset($_POST['name']) || !isset($_POST['email']) || !isset($_POST['message'])) {
+		died('We are sorry, but there appears to be a problem with the form you submitted.');
+	}
+
+	$email_to = 'info@adrianeyre.co.uk';
+	$email_subject = 'Message from portfolio site';
+	$name = $_POST['name'];
+	$email_from = $_POST['email'];
+	$message = $_POST['message'];
+	$error_message = "";
+
+	if (strlen($message) < 2) {
+		$error_message .= 'The Comments you entered do not appear to be valid.<br />';
+	}
+
+	if (strlen($error_message) > 0) {
+		died($error_message);
+	}
+
+	$email_message = "Form details below.\n\n";
+	$email_message .= "Name: ".clean_string($email)."\n";
+	$email_message .= "Email: ".clean_string($email_from)."\n";
+	$email_message .= "Message: ".clean_string($message)."\n";
+
+	$headers = 'From: '.$email_from."\r\n".
+	'Reply-To: '.$email_from."\r\n" .
+	'X-Mailer: PHP/' . phpversion();
+
+	@mail($email_to, $email_subject, $email_message, $headers);
 ?>
-     
-    Thank you for contacting us. We will be in touch with you very soon.
-     
-<?php
-    }
-?>
+
+	<div>Thank you for contacting us. We will be in touch with you very soon.</div>
